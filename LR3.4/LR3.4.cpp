e6d0b459ccaf07f9e28d31d9eeb9916724eb3add
Oleksandr Kovalchuk <oleksandr.kovalchuk.kn@dspu.edu.ua>
2025-11-12 19:44:36 +0200

    final2

--- LR3.4/LR3.4.cpp
+++ LR3.4/LR3.4.cpp
@@ -18,25 +18,19 @@ int main()
     cout << "y = "; cin >> y;
 
     // Параметри кіл
-    double a;
-    double b;
     double R;
 
-    cout << "a = "; cin >> a;
-    cout << "b = "; cin >> b;
     cout << "R = "; cin >> R;
 
-    // Перевірка сірого кольору (виправлено згідно з рисунком)
+    // Перевірка сірого кольору
     bool inGray =
-
-        (pow(x - a, 2) + pow(y - b, 2) <= pow(R, 2) && y > x)
-                                 ||
-        (pow(x + a, 2) + pow(y + b, 2) <= pow(R, 2) && y < x);
+        ((pow(x - R, 2) + pow(y - R, 2) <= pow(R, 2)) && (y <= x))
+        ||
+        ((pow(x + R, 2) + pow(y + R, 2) <= pow(R, 2)) && (y >= x));
     if (inGray)
         cout << "Yes" << endl;
     else
         cout << "No" << endl;
-    cin.get();
-    cin.get();
+ 
     return 0;
 }
